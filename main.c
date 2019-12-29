95b150c402af6485ec7dfc0d0cfdc04e68a2d36d
sherlockomar <sherlockomar89@gmail.com>
2019-12-29 11:16:24 +0100

    Added list parsing printing lists

--- main.c
+++ main.c
@@ -43,6 +43,7 @@ typedef struct {
 typedef struct {
 
     type_value *value;
+    unsigned long number_of_list_key_values;
     
 } list;
 
@@ -58,10 +59,12 @@ typedef struct {
 
 //Prototypes
 json *get_json_parts(FILE *file);
+list *get_list_parts(FILE *file);
 char *get_string(FILE *file, char c);
+parsed_number *get_number(FILE *file, char c);
 void add_tabs(unsigned n);
 void print_json(const json *json_parsed, unsigned level);
-parsed_number *get_number(FILE *file, char c);
+void print_list(const list *list_parsed, unsigned level);
 
 //Main Program
 int main(void){
@@ -241,12 +244,177 @@ json *get_json_parts(FILE *file){
                         } 
                     }
                     else if (c == '['){
-
+                        partial_json->json_key_values[partial_json->number_of_json_key_values-1].value.type = jsonList;
+                        partial_json->json_key_values[partial_json->number_of_json_key_values-1].value.value = (list*) malloc(sizeof(list));
+                        
+                        fseek(file, -1 , SEEK_CUR);
+                        partial_json->json_key_values[partial_json->number_of_json_key_values-1].value.value = (list*) get_list_parts(file);
+                        
+                        c = fgetc(file);
+                        while(c == ' ' || c == '\t' || c == '\n')c = fgetc(file);
                     }
                 }while(c == ',');
             }
         }while(c == ',');
     return partial_json;
+}
+
+list *get_list_parts(FILE *file){
+    char c;
+    unsigned short char_counter = 0;
+    list *partial_list;
+    type_value *p;
+
+    partial_list = (list*) malloc(sizeof(list));
+    partial_list->number_of_list_key_values = 0;
+        c = fgetc(file);
+        while(c == ' ' || c == '\t' || c == '\n')c = fgetc(file);
+        do{
+            if (c == '[')
+            {
+                do{
+                    partial_list->number_of_list_key_values++;
+                    p = (type_value *) realloc(partial_list->value, partial_list->number_of_list_key_values * sizeof(type_value));
+                    if(p){
+                        partial_list->value = p;
+                    }
+                    else{
+                        printf("reallocation faild\n");
+                        assert(p == NULL);
+                    }
+
+                    c = fgetc(file);
+                    while(c == ' ' || c == '\t' || c == '\n')c = fgetc(file);
+                    assert(c != EOF);
+                    assert(c != ',');
+
+                    
+                    if( c == '\"'){
+                        partial_list->value[partial_list->number_of_list_key_values-1].type = string;
+                        partial_list->value[partial_list->number_of_list_key_values-1].value = (char*) malloc(sizeof(char));
+                        
+                        c = fgetc(file);
+                        strcpy(partial_list->value[partial_list->number_of_list_key_values-1].value, get_string(file,c));
+                        
+                        c = fgetc(file);
+                        while(c == ' ' || c == '\t' || c == '\n')c = fgetc(file);
+                        assert(c != EOF);
+
+                        if(c == ']'){
+                            return partial_list;
+                        }
+                    }
+                    else if( c == '{'){
+                        partial_list->value[partial_list->number_of_list_key_values-1].type = jsonObj;
+                        partial_list->value[partial_list->number_of_list_key_values-1].value = (json*) malloc(sizeof(json));
+                        
+                        fseek(file, -1 , SEEK_CUR);
+                         partial_list->value[partial_list->number_of_list_key_values-1].value = (json*) get_json_parts(file);
+                        
+                        c = fgetc(file);
+                        while(c == ' ' || c == '\t' || c == '\n')c = fgetc(file);
+                    }
+                    else if( c == '-' || (c >= '0' && c <= '9')){
+                        parsed_number *number = (parsed_number*) malloc(sizeof(parsed_number));
+
+                        if(c == '-'){
+                            c = fgetc(file);
+                            assert((c >= '0' && c <= '9') || c == '.');
+                            number = get_number(file, c);
+                            if(number->isDecimal){
+                                 partial_list->value[partial_list->number_of_list_key_values-1].type = decimal;
+                                 partial_list->value[partial_list->number_of_list_key_values-1].value = (double*) malloc(sizeof(double));
+                                *((double*) partial_list->value[partial_list->number_of_list_key_values-1].value) = (double) atof(number->number) * -1.0f;
+                            }
+                            else{
+                                 partial_list->value[partial_list->number_of_list_key_values-1].type = integer;
+                                 partial_list->value[partial_list->number_of_list_key_values-1].value = (long*) malloc(sizeof(long));
+                                *((long*) partial_list->value[partial_list->number_of_list_key_values-1].value) = (long) atoi(number->number) * -1;
+                            }
+                        }
+                        else{
+                            assert((c >= '0' && c <= '9') || c == '.');
+                            number = get_number(file, c);
+                            if(number->isDecimal){
+                                partial_list->value[partial_list->number_of_list_key_values-1].type = decimal;
+                                partial_list->value[partial_list->number_of_list_key_values-1].value = (double*) malloc(sizeof(double));
+                                *((double*) partial_list->value[partial_list->number_of_list_key_values-1].value) = (double) atof(number->number);
+                            }
+                            else{
+                                partial_list->value[partial_list->number_of_list_key_values-1].type = integer;
+                                partial_list->value[partial_list->number_of_list_key_values-1].value = (long*) malloc(sizeof(long));
+                                *((long*) partial_list->value[partial_list->number_of_list_key_values-1].value) = (long) atoi(number->number);
+                            }
+                        }
+
+                        c = fgetc(file);
+                        while(c == ' ' || c == '\t' || c == '\n')c = fgetc(file);
+                        assert(c != EOF);
+
+                        if(c == ']'){
+                            return partial_list;
+                        } 
+                    }
+                    else if(c == 'n' || c == 't' || c == 'f'){
+                        if(c == 'n' || c == 't'){
+                            char word[4];
+                            for(size_t i = 0 ; i < 4 ; i++){
+                                assert(c == 'n' || c == 'u' || c == 'l' || c == 't' || c == 'r' || c == 'e');
+                                word[i] = c;
+                                c = fgetc(file); 
+                            }
+                            if(strncmp("null", word, 4) == 0){
+                                partial_list->value[partial_list->number_of_list_key_values-1].type = null;
+                                partial_list->value[partial_list->number_of_list_key_values-1].value = NULL;
+                            }
+                            else if(strncmp("true", word, 4) == 0){
+                                partial_list->value[partial_list->number_of_list_key_values-1].type = boolean;
+                                partial_list->value[partial_list->number_of_list_key_values-1].value = (bool*) malloc(sizeof(bool));
+                                *((bool*)partial_list->value[partial_list->number_of_list_key_values-1].value) = true;
+                            }
+                            else{
+                                printf(".......File syntax error......\n");
+                            }
+                            
+                        }
+                        else if(c == 'f'){
+                            char word[5];
+                            for(size_t i = 0 ; i < 5 ; i++){
+                                assert(c == 'f' || c == 'a' || c == 'l' || c == 's' || c == 'e');
+                                word[i] = c;
+                                c = fgetc(file); 
+                            }
+                            if(strncmp("false", word, 5) == 0){
+                                partial_list->value[partial_list->number_of_list_key_values-1].type = boolean;
+                                partial_list->value[partial_list->number_of_list_key_values-1].value = (bool*) malloc(sizeof(bool));
+                                *((bool*)partial_list->value[partial_list->number_of_list_key_values-1].value) = false;
+                            }
+                            else{
+                                printf(".......File syntax error......\n");
+                            }
+                        }
+
+                        while(c == ' ' || c == '\t' || c == '\n')c = fgetc(file);
+                        assert(c != EOF);
+
+                        if(c == ']'){
+                            return partial_list;
+                        } 
+                    }
+                    else if (c == '['){
+                        partial_list->value[partial_list->number_of_list_key_values-1].type = jsonList;
+                        partial_list->value[partial_list->number_of_list_key_values-1].value = (list*) malloc(sizeof(list));
+                        
+                        fseek(file, -1 , SEEK_CUR);
+                        partial_list->value[partial_list->number_of_list_key_values-1].value = (list*) get_list_parts(file);
+                        
+                        c = fgetc(file);
+                        while(c == ' ' || c == '\t' || c == '\n')c = fgetc(file);
+                    }
+                }while(c == ',');
+            }
+        }while(c == ',');
+    return partial_list;
 }
 
 char *get_string(FILE *file, char c){
@@ -314,10 +482,31 @@ void print_json(const json *json_parsed, unsigned level){
             case integer : add_tabs(level);printf("\t\"%s\" : %d,\n", json_parsed->json_key_values[i].key, *(long*)json_parsed->json_key_values[i].value.value);break;
             case decimal : add_tabs(level);printf("\t\"%s\" : %f,\n", json_parsed->json_key_values[i].key, *(double*)json_parsed->json_key_values[i].value.value);break;
             case boolean : add_tabs(level);printf("\t\"%s\" : %s,\n", json_parsed->json_key_values[i].key, *((bool*)json_parsed->json_key_values[i].value.value)?"true":"false");break;
+            case jsonList : add_tabs(level);printf("\t\"%s\" : ",json_parsed->json_key_values[i].key);print_list(json_parsed->json_key_values[i].value.value, level+1);break;
             case jsonObj : add_tabs(level);printf("\t\"%s\" : ",json_parsed->json_key_values[i].key);print_json(json_parsed->json_key_values[i].value.value, level+1);break;
             default : printf("......Printing error......");break;
         }
     }
     add_tabs(level);
     printf("},\n");
+}
+
+void print_list(const list *list_parsed, unsigned level){
+    size_t i = 0;
+    //add_tabs(level);
+    printf("[\n");
+    for(i = 0 ; i < list_parsed->number_of_list_key_values ; i++){
+        switch(list_parsed->value[i].type){
+            case null : add_tabs(level);printf("\tnull,\n");break;
+            case string : add_tabs(level);printf("\t\"%s\",\n", list_parsed->value[i].value);break;
+            case integer : add_tabs(level);printf("\t%d,\n", *(long*)list_parsed->value[i].value);break;
+            case decimal : add_tabs(level);printf("\t%f,\n", *(double*)list_parsed->value[i].value);break;
+            case boolean : add_tabs(level);printf("\t%s,\n", *((bool*)list_parsed->value[i].value)?"true":"false");break;
+            case jsonList : add_tabs(level);print_list(list_parsed->value[i].value, level+1);break;
+            case jsonObj : add_tabs(level);print_json(list_parsed->value[i].value, level+1);break;
+            default : printf("......Printing error......");break;
+        }
+    }
+    add_tabs(level);
+    printf("],\n");
 }
