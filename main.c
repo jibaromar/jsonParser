5fd2184387dea3a4030d60d013f1daa238525418
sherlockomar <sherlockomar89@gmail.com>
2019-12-29 02:41:18 +0100

    Added type_value structure

--- main.c
+++ main.c
@@ -7,14 +7,14 @@
 #define JSON_FILE_PATH "DATA/file.json"
 
 //Data structurs
-typedef enum bool {
+typedef enum {
 
     false,
     true
 
 } bool;
 
-typedef enum value_type {
+typedef enum {
 
     null,
     string,
@@ -24,17 +24,23 @@ typedef enum value_type {
     list,
     jsonObj
 
-} value_type;
+} value_types;
 
-typedef struct key_value {
+typedef struct {
+
+    value_types type;
+    void *value;
+
+} type_value;
+
+typedef struct {
 
     char *key;
-    value_type type;
-    void *value;
+    type_value *value;
     
 } key_value;
 
-typedef struct json {
+typedef struct {
     key_value *json_key_values;
     unsigned long number_of_json_key_values;
 } json;
@@ -90,11 +96,13 @@ json *get_json_parts(FILE *file){
                     p = (key_value *) realloc(partial_json->json_key_values, partial_json->number_of_json_key_values * sizeof(key_value));
                     if(p){
                         partial_json->json_key_values = p;
+                        partial_json->json_key_values[partial_json->number_of_json_key_values-1].value = (type_value*) malloc(sizeof(type_value));
                     }
                     else{
                         printf("reallocation faild\n");
                         assert(p == NULL);
                     }
+
                     c = fgetc(file);
                     while(c == ' ' || c == '\t' || c == '\n')c = fgetc(file);
                     assert(c != EOF);
@@ -116,11 +124,11 @@ json *get_json_parts(FILE *file){
 
                     
                     if( c == '\"'){
-                        partial_json->json_key_values[partial_json->number_of_json_key_values-1].type = string;
-                        partial_json->json_key_values[partial_json->number_of_json_key_values-1].value = (char*) malloc(sizeof(char));
+                        partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->type = string;
+                        partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->value = (char*) malloc(sizeof(char));
                         
                         c = fgetc(file);
-                        strcpy(partial_json->json_key_values[partial_json->number_of_json_key_values-1].value, get_string(file,c));
+                        strcpy(partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->value, get_string(file,c));
                         
                         c = fgetc(file);
                         while(c == ' ' || c == '\t' || c == '\n')c = fgetc(file);
@@ -131,11 +139,11 @@ json *get_json_parts(FILE *file){
                         }
                     }
                     else if( c == '{'){
-                        partial_json->json_key_values[partial_json->number_of_json_key_values-1].type = jsonObj;
-                        partial_json->json_key_values[partial_json->number_of_json_key_values-1].value = (json*) malloc(sizeof(json));
+                        partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->type = jsonObj;
+                        partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->value = (json*) malloc(sizeof(json));
                         
                         fseek(file, -1 , SEEK_CUR);
-                        partial_json->json_key_values[partial_json->number_of_json_key_values-1].value = (json*) get_json_parts(file);
+                        partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->value = (json*) get_json_parts(file);
                         
                         c = fgetc(file);
                         while(c == ' ' || c == '\t' || c == '\n')c = fgetc(file);
@@ -148,28 +156,28 @@ json *get_json_parts(FILE *file){
                             assert((c >= '0' && c <= '9') || c == '.');
                             number = get_number(file, c);
                             if(number->isDecimal){
-                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].type = decimal;
-                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value = (double*) malloc(sizeof(double));
-                                *((double*)partial_json->json_key_values[partial_json->number_of_json_key_values-1].value) = (double) atof(number->number) * -1.0f;
+                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->type = decimal;
+                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->value = (double*) malloc(sizeof(double));
+                                *((double*)partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->value) = (double) atof(number->number) * -1.0f;
                             }
                             else{
-                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].type = integer;
-                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value = (long*) malloc(sizeof(long));
-                                *((long*)partial_json->json_key_values[partial_json->number_of_json_key_values-1].value) = (long) atoi(number->number) * -1;
+                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->type = integer;
+                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->value = (long*) malloc(sizeof(long));
+                                *((long*)partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->value) = (long) atoi(number->number) * -1;
                             }
                         }
                         else{
                             assert((c >= '0' && c <= '9') || c == '.');
                             number = get_number(file, c);
                             if(number->isDecimal){
-                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].type = decimal;
-                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value = (double*) malloc(sizeof(double));
-                                *((double*)partial_json->json_key_values[partial_json->number_of_json_key_values-1].value) = (double) atof(number->number);
+                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->type = decimal;
+                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->value = (double*) malloc(sizeof(double));
+                                *((double*)partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->value) = (double) atof(number->number);
                             }
                             else{
-                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].type = integer;
-                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value = (long*) malloc(sizeof(long));
-                                *((long*)partial_json->json_key_values[partial_json->number_of_json_key_values-1].value) = (long) atoi(number->number);
+                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->type = integer;
+                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->value = (long*) malloc(sizeof(long));
+                                *((long*)partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->value) = (long) atoi(number->number);
                             }
                         }
 
@@ -190,13 +198,13 @@ json *get_json_parts(FILE *file){
                                 c = fgetc(file); 
                             }
                             if(strncmp("null", word, 4) == 0){
-                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].type = null;
-                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value = NULL;
+                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->type = null;
+                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->value = NULL;
                             }
                             else if(strncmp("true", word, 4) == 0){
-                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].type = boolean;
-                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value = (bool*) malloc(sizeof(bool));
-                                *((bool*)partial_json->json_key_values[partial_json->number_of_json_key_values-1].value) = true;
+                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->type = boolean;
+                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->value = (bool*) malloc(sizeof(bool));
+                                *((bool*)partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->value) = true;
                             }
                             else{
                                 printf(".......File syntax error......\n");
@@ -211,9 +219,9 @@ json *get_json_parts(FILE *file){
                                 c = fgetc(file); 
                             }
                             if(strncmp("false", word, 5) == 0){
-                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].type = boolean;
-                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value = (bool*) malloc(sizeof(bool));
-                                *((bool*)partial_json->json_key_values[partial_json->number_of_json_key_values-1].value) = false;
+                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->type = boolean;
+                                partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->value = (bool*) malloc(sizeof(bool));
+                                *((bool*)partial_json->json_key_values[partial_json->number_of_json_key_values-1].value->value) = false;
                             }
                             else{
                                 printf(".......File syntax error......\n");
@@ -227,9 +235,9 @@ json *get_json_parts(FILE *file){
                             return partial_json;
                         } 
                     }
-                    // else if (c == '['){
+                    else if (c == '['){
 
-                    // }
+                    }
                 }while(c == ',');
             }
         }while(c == ',');
@@ -249,7 +257,8 @@ char *get_string(FILE *file, char c){
                 
         c = fgetc(file);
     }
-
+        
+    //Add \0 caracter on key
     string = (char *)realloc(string,(char_counter+1)*sizeof(char));
     string[char_counter] = '\0';
 
@@ -273,7 +282,8 @@ parsed_number *get_number(FILE *file, char c){
                 
         c = fgetc(file);
     }
-
+    
+    //Add \0 caracter on key
     number->number = (char *)realloc(number->number,(char_counter+1)*sizeof(char));
     number->number[char_counter] = '\0';
     fseek(file, -1, SEEK_CUR);
@@ -293,13 +303,13 @@ void print_json(const json *json_parsed, unsigned level){
     //add_tabs(level);
     printf("{\n");
     for(i = 0 ; i < json_parsed->number_of_json_key_values ; i++){
-        switch(json_parsed->json_key_values[i].type){
+        switch(json_parsed->json_key_values[i].value->type){
             case null : add_tabs(level);printf("\t\"%s\" : null,\n", json_parsed->json_key_values[i].key);break;
-            case string : add_tabs(level);printf("\t\"%s\" : \"%s\",\n", json_parsed->json_key_values[i].key, json_parsed->json_key_values[i].value);break;
-            case integer : add_tabs(level);printf("\t\"%s\" : %d,\n", json_parsed->json_key_values[i].key, *(long*)json_parsed->json_key_values[i].value);break;
-            case decimal : add_tabs(level);printf("\t\"%s\" : %f,\n", json_parsed->json_key_values[i].key, *(double*)json_parsed->json_key_values[i].value);break;
-            case boolean : add_tabs(level);printf("\t\"%s\" : %s,\n", json_parsed->json_key_values[i].key, *((bool*)json_parsed->json_key_values[i].value)?"true":"false");break;
-            case jsonObj : add_tabs(level);printf("\t\"%s\" : ",json_parsed->json_key_values[i].key);print_json(json_parsed->json_key_values[i].value, level+1);break;
+            case string : add_tabs(level);printf("\t\"%s\" : \"%s\",\n", json_parsed->json_key_values[i].key, json_parsed->json_key_values[i].value->value);break;
+            case integer : add_tabs(level);printf("\t\"%s\" : %d,\n", json_parsed->json_key_values[i].key, *(long*)json_parsed->json_key_values[i].value->value);break;
+            case decimal : add_tabs(level);printf("\t\"%s\" : %f,\n", json_parsed->json_key_values[i].key, *(double*)json_parsed->json_key_values[i].value->value);break;
+            case boolean : add_tabs(level);printf("\t\"%s\" : %s,\n", json_parsed->json_key_values[i].key, *((bool*)json_parsed->json_key_values[i].value->value)?"true":"false");break;
+            case jsonObj : add_tabs(level);printf("\t\"%s\" : ",json_parsed->json_key_values[i].key);print_json(json_parsed->json_key_values[i].value->value, level+1);break;
             default : printf("......Printing error......");break;
         }
     }
